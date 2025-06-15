e2eef570a0fc249ca1286e811fb85f2f280bf94b
Eden Abwe <edenjardin089@gmail.com>
2025-06-15 05:51:38 -0400

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,23 +25,27 @@ const useGames = () => {
   // Hook logic here
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     // handling cancellation with abort controller
     const controller = new AbortController();
     const signal = controller.signal;
     // Fetch games from the API
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal })
       .then((res) => {
         setGames(res.data.results);
+        setLoading(false);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
     return () => controller.abort();
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 export default useGames;
