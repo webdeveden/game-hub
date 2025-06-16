4ee315a2df43e5f46855bd53817cbdf4d5202180
Eden Abwe <edenjardin089@gmail.com>
2025-06-15 23:45:31 -0400

    Fetching by Genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Genres {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {
+  count: number;
+  results: Genres[];
+}
+
+const useGenres = () => {
+  // Hook logic here
+  const [genres, setGenres] = useState<Genres[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // handling cancellation with abort controller
+    const controller = new AbortController();
+    const signal = controller.signal;
+    // Fetch games from the API
+    setLoading(true);
+    apiClient
+      .get<FetchGenresResponse>("/genres", { signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
+  return { genres, error, isLoading };
+};
+export default useGenres;
