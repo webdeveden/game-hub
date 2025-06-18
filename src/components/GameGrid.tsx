16539314a5dc06fb1d2ae2d7d6b2dae027fbcc9b
Eden Abwe <edenjardin089@gmail.com>
2025-06-18 05:37:21 -0400

    Displaying a message when there is data associated with selected platform

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,12 @@ function GameGrid({ selectedGenre, selectedPlatform }: Props) {
   const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
   const skeletons = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16];
   //   if (error) return <Text>{error}</Text>;
-  //   if (games.length === 0) return <Text>Loading...</Text>;
+  if (data.length === 0)
+    return (
+      <Text>
+        No data found with: {selectedPlatform?.name || "None"} Platform
+      </Text>
+    );
   return (
     <>
       {error && <Text>Error: {error}</Text>}
