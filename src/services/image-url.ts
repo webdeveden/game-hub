c3da82367bc302f44915dcb3e1b60ca02fc2bf7a
Eden Abwe <edenjardin089@gmail.com>
2025-06-19 02:22:12 -0400

    Handle with ni Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
 // image optimazation function on the image url
 
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage; // handling games without images
   const target = "media/";
   const imageOpt = "crop/600/400/";
   const index = url.indexOf(target) + target.length;
