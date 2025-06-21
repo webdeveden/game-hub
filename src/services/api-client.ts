e7b056d491cedca1bac5a1ead446c83031c6871a
Eden Abwe <edenjardin089@gmail.com>
2025-06-21 06:25:14 -0400

    Hidding API key in .env file then gitignore it

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const apiKey = import.meta.env.VITE_RAWG_API_KEY;
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "af1e8e5a730b4a88bcb953e96c02832e",
+    key: apiKey,
   },
 });
