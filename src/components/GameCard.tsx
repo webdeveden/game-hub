ada1bc69d25ea59e3f87133c70f104e54a799ee9
Eden Abwe <edenjardin089@gmail.com>
2025-06-21 00:24:01 -0400

    Clean up game grid

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,7 +13,6 @@ function GameCard({ game }: GameCardProps) {
     <Card borderRadius={10} overflow={"hidden"}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
         <HStack justifyContent="space-between">
           <PlatformIconsList
             platforms={game.parent_platforms.map(
@@ -22,6 +21,7 @@ function GameCard({ game }: GameCardProps) {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
       </CardBody>
     </Card>
   );
